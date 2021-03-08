4d8f6f653e9fb2089cd8d1fa7e4a3dfe77b58ec0
bestseob93 <bestseob93@gmail.com>
2021-03-09 00:51:19 +0900

    :wrench: add constants file

---
+++ src/constants.ts
@@ -0,0 +1,35 @@
+export const VIEW_TYPE = {
+  DAY: 'day',
+  WEEK: 'week',
+  MONTH: 'month'
+};
+
+export const DATE_TYPE = {
+  MINUTES: 'minutes',
+  HOUR: 'hour',
+  DAY: 'day'
+};
+
+export const HALF_AN_HOUR = 30;
+
+export const TABLE_CONTENT_CELL_WIDTH = {
+  DAY_CELL_WIDTH: 20,
+  WEEK_CELL_WIDTH: 20,
+  MONTH_CELL_WIDTH: 40
+};
+
+export const TABLE_DATE_CELL_WIDTH = {
+  DAY_CELL_WIDTH: 80,
+  WEEK_CELL_WIDTH: 60,
+  MONTH_CELL_WIDTH: 40
+};
+
+export const DAY_COLSPAN = 24;
+export const WEEK_COLSPAN = 8;
+
+export const WEEK_STEP = 3;
+export const DAY_STEP = 15;
+
+export const TH_CELL_WIDTH = 40;
+export const TABLE_ROW_HEIGHT = 40;
+export const EVENT_ITEM_HEIGHT = 17;
